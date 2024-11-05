70d39b3ca49684054fdc40e6e1af2719bb3e6bab
Richard Partain <rdprtin1@memphis.edu>
2024-11-05 10:12:24 -0600

    cleanup, formatting

--- main.c
+++ main.c
@@ -7,57 +7,6 @@
 #include "directory_operations.h"
 #include "utility.h"
 
-// char ** tokenize(char * input, const char * delim, const size_t maxTokens);
-
-// void changeDirectory(char ** args);
-// void listDirectory(const char * path);
-// void touch(char * fileName);
-
-
-
-
-// int handle_command_if_else(char** args) {
-//     if (strcmp(args[0], "help") == 0) {
-//         printf("Showing help menu\n");
-//         showHelp();
-//         return 0;
-//     } else if (strcmp(args[0], "ls") == 0) {
-//         printf("Listing items\n");
-//         const char *path = args[1] ? args[1] : ".";  // Use current directory if no argument
-//         listDirectory(path);
-//         return 0;
-//     } else if (strcmp(args[0], "cd") == 0){
-//         changeDirectory(args);
-//         return 0;
-//     } else if (strcmp(args[0], "echo") == 0){
-//         if (args[1] != NULL) {
-//             int i = 1;
-//             while (args[i] != NULL) {
-//                 printf("%s ", args[i]);  // Print each argument
-//                 i++;
-//             }
-//         }
-//         puts("");
-//         return 0;
-//     } else if (strcmp(args[0], "exit") == 0) {
-//         printf("Exiting program\n");
-//         return 1;
-//     } else if (strcmp(args[0], "touch") == 0){
-//         if (args[1] != NULL) {
-//             touch(args[1]);  // Call touchFile with the specified filename
-//             return 0;
-//         } else {
-//             printf("touch: missing file operand\n");
-//         }
-//     } else {
-//         printf("\nUnknown command: %s\n", args[0]);
-//         showHelp();
-//         return 0;
-//     }
-// }
-
-
-
 
 // Main //
 int main(void) {
